8c2e8d41fb881ad299fc98f28fe2ef4f07e70288
Areeb Khan <55325380+areebkhan274427@users.noreply.github.com>
2023-04-20 10:07:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+var input = document.getElementById('evaluatedText');
+var text = document.getElementById("letterCount");
+
+function myFunction(){
+	var length = input.value.length;
+
+	text.innerText=length;
+	
+}
